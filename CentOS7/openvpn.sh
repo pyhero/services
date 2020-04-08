6987921403fa45dcf7bf31b03a720c4928536314
Panda <xujianhua@sensorsdata.cn>
2020-04-08 12:51:09 +0800

    iptables

--- CentOS7/openvpn.sh
+++ CentOS7/openvpn.sh
@@ -24,6 +24,22 @@ echo 'net.netfilter.nf_conntrack_max = 655350' >> /etc/sysctl.conf
 sysctl -p
 
 yum install -y iptables-services
-iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.0.0/16 -j MASQUERADE
 service iptables save
 systemctl enable iptables
+
+# rd
+iptables -t filter -N RD
+iptables -t filter -F RD
+iptables -t filter -A RD -s 192.168.72.0/21 -d 10.120.0.0/16 -o eth0 -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -t filter -A RD -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -t filter -A FORWARD -s 192.168.72.0/21 -j RD
+iptables -t filter -A FORWARD -s 192.168.72.0/21 -d 10.0.0.0/8 -o eth0 -p tcp -m tcp --dport 22 -j REJECT --reject-with icmp-port-unreachable
+
+# admin
+iptables -t filter -N ADMIN
+iptables -t filter -F ADMIN
+iptables -t filter -A ADMIN -s 192.168.80.0/21 -d 10.10.96.80/32 -o eth0 -p tcp -m tcp --dport 443 -j ACCEPT
+iptables -t filter -A ADMIN -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -t filter -A FORWARD -s 192.168.80.0/21 -j ADMIN
+iptables -t filter -A FORWARD -s 192.168.80.0/21 -o eth0 -j REJECT --reject-with icmp-host-prohibited
